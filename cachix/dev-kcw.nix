ea27ffce7b37fd699a9a00dfad7ebcf2fbcde1b6
KCW <dev@kcwesley.uk>
2020-12-14 12:46:27 +0100

    add dev-kcw cachix file

---
+++ cachix/dev-kcw.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://dev-kcw.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "dev-kcw.cachix.org-1:Dbw5xmEmj+jim3+7CBSoOsFYpG/lk2b7be009+XYtX4="
+    ];
+  };
+}
